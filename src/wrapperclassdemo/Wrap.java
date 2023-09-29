a21b6d7e817eac6398fd92a0cc65fe67bc53cfbe
PAAG <keapaagdat23a@gmail.com>
2023-09-29 10:11:03 +0200

    ok

--- src/wrapperclassdemo/Wrap.java
+++ src/wrapperclassdemo/Wrap.java
@@ -6,11 +6,10 @@ public class Wrap {
   public static void main(String[] args) {
     String s = "17";
     int c = s.charAt(0);
-    int i = Integer.parseInt(s);
+    int i = Integer.parseInt(s); // Integer example
     System.out.println(i);
 
-
-    double d = Double.parseDouble("24");
+    double d = Double.parseDouble("24"); // Double example
 
     ArrayList<Integer> numbers = new ArrayList(); // Not int, but Integer
   }
