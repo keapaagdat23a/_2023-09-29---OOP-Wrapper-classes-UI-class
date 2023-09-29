18cca13156ee37c98dc150c562d58716f010fb7d
PAAG <keapaagdat23a@gmail.com>
2023-09-29 10:06:23 +0200

    ok

--- src/wrapperclassdemo/Wrap.java
+++ src/wrapperclassdemo/Wrap.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class Wrap {
   public static void main(String[] args) {
-    String s = "17 ";
+    String s = "17";
     int c = s.charAt(0);
     int i = Integer.parseInt(s);
     System.out.println(i);
-    Double.parseDouble();
 
+    double d = Double.parseDouble("24");
 
-    ArrayList<Integer> numbers = new ArrayList();
+    ArrayList<Integer> numbers = new ArrayList(); // Not int, but Integer
   }
 }
